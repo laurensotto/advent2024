aaf8bca97567c61a2d1db66907356f91eac9e48e
Laurens Otto <info@rensotto.nl>
2024-12-12 23:06:48 +0100

    Day 11 part 2

--- internal/day11/day11.go
+++ internal/day11/day11.go
@@ -2,6 +2,7 @@ package day11
 
 import (
 	"fmt"
+	"github.com/laurensotto/advent2024/pkg/sliceutil"
 	"log"
 	"strconv"
 	"strings"
@@ -10,13 +11,14 @@ import (
 
 func Solve(input string, verbose bool) (string, int64, string, int64) {
 	splitString := strings.Split(input, " ")
+	copiedString := sliceutil.DeepCopySlice(splitString)
 
 	startTime1 := time.Now()
 	part1Result := part1(splitString, verbose)
 	time1Result := time.Since(startTime1).Milliseconds()
 
 	startTime2 := time.Now()
-	part2Result := part2(splitString, verbose)
+	part2Result := part2(copiedString, verbose)
 	time2Result := time.Since(startTime2).Milliseconds()
 
 	return strconv.Itoa(part1Result), time1Result, strconv.Itoa(part2Result), time2Result
@@ -84,5 +86,65 @@ func blink(input []string, verbose bool) []string {
 }
 
 func part2(input []string, verbose bool) int {
-	return 0
+	stones := make(map[int]int)
+
+	for _, value := range input {
+		intValue, err := strconv.Atoi(value)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		stones[intValue] += 1
+	}
+
+	for i := 0; i < 75; i++ {
+		stones = blinkOptimized(stones, verbose)
+	}
+
+	var totalStones int
+
+	for _, countOfStones := range stones {
+		totalStones += countOfStones
+	}
+
+	return totalStones
+}
+
+func blinkOptimized(stones map[int]int, verbose bool) map[int]int {
+	if verbose {
+		fmt.Println(stones)
+	}
+
+	newStones := make(map[int]int)
+
+	for stoneValue, stoneCount := range stones {
+		if stoneValue == 0 {
+			newStones[1] += stoneCount
+			continue
+		}
+
+		stringStoneValue := strconv.Itoa(stoneValue)
+
+		if len(stringStoneValue)%2 == 0 {
+			mid := len(stringStoneValue) / 2
+			firstHalf, firstErr := strconv.Atoi(stringStoneValue[:mid])
+			secondHalf, secondErr := strconv.Atoi(stringStoneValue[mid:])
+
+			if firstErr != nil {
+				log.Fatal(firstErr)
+			}
+
+			if secondErr != nil {
+				log.Fatal(secondErr)
+			}
+
+			newStones[firstHalf] += stoneCount
+			newStones[secondHalf] += stoneCount
+		} else {
+			newStones[stoneValue*2024] += stoneCount
+		}
+	}
+
+	return newStones
 }
